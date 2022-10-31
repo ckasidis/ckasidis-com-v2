a53c10a10a03d61f9977c71f5038c0d3d2db1703
Kasidis Chantharojwong <me@ckasidis.com>
2022-10-31 18:13:07 +0800

    add content to projects page

---
+++ src/data/projects.ts
@@ -0,0 +1,16 @@
+export const projects = [
+	{
+		competition: `1st Runner-up at Crypto.com "The Next Gen" Web3 Hackathon 2022`,
+		title: 'TickETH',
+		description: `In two days, I led a team of 5 to develop a solution called “TickETH”, an NFT Ticketing Platform built with Ethereum, Hardhat, Next.js, and PostgreSQL. Our platform TickETH allows event organizers to deploy their smart contracts and manage events directly through our dashboard UI. At the same time, ticket buyers can easily browse through events and buy tickets in the form of NFTs.`,
+		image: '/ticketh.png',
+		link: 'https://github.com/ckasidis/ticketh_cdc-hackathon_team-drop-table',
+	},
+	{
+		competition: `Open source truth or dare web app`,
+		title: 'Truth or Dare',
+		description: `I developed a truth or dare web app using Next.js, React Query, Chakra UI, Zustand, and Truth or Dare API. It is also containerized with Docker and deployed to production with fly.io. The app lets you control game settings like players and game mode. You can then play the truth or dare game by pressing reroll buttons to select a player and question randomly.`,
+		image: '/tod-app.png',
+		link: 'https://github.com/ck-oss/tod-app',
+	},
+];
