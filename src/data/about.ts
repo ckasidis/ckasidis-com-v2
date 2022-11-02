8c63e3e6c00508f9df997ae0ca41e38340e28571
Kasidis Chantharojwong <me@ckasidis.com>
2022-11-02 19:28:39 +0800

    update about page content

---
+++ src/data/about.ts
@@ -0,0 +1,25 @@
+import { SiGithub, SiGmail, SiInstagram, SiLinkedin } from 'react-icons/si';
+
+export const socialLinks = [
+	{
+		text: 'Follow on GitHub',
+		href: 'https://github.com/ckasidis',
+		icon: SiGithub,
+	},
+	{
+		text: 'Follow on LinkedIn',
+		href: 'https://www.linkedin.com/in/ckasidis/',
+		icon: SiLinkedin,
+	},
+	{
+		text: 'Follow on Instagram',
+		href: 'https://www.instagram.com/knot.ck/',
+		icon: SiInstagram,
+	},
+];
+
+export const mail = {
+	text: 'me@ckasidis.com',
+	href: 'mailto:me@ckasidis.com',
+	icon: SiGmail,
+};
