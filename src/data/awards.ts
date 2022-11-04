c2905a210faabc54b17f24f1f46fd71b879f3044
Kasidis Chantharojwong <me@ckasidis.com>
2022-11-05 07:59:11 +0800

    add awards page content

---
+++ src/data/awards.ts
@@ -0,0 +1,40 @@
+export const awards = [
+	{
+		date: '2022 October',
+		title: '1st Runner-up - Crypto.com "The Next Gen" Hackathon',
+		description:
+			'Led a team of 5 to develop a Full Stack Web3 Application in 48 hours. Our team built an NFT Ticketing Platform and won the second prize.',
+	},
+	{
+		date: '2020 March',
+		title: 'ASEAN Undergraduate Scholarship - NTU',
+		description: 'An ASEAN Undergraduate Scholar at NTU from 2020-2024',
+	},
+	{
+		date: '2019 August',
+		title:
+			'Silver Medal - 13th International Olympiad on Astronomy and Astrophysics',
+		description:
+			'Won a Silver Medal at the 13th International Olympiad on Astronomy and Astrophysics in 2019 at Keszthely, Hungary.',
+	},
+	{
+		date: '2019 May',
+		title: 'Gold Medal - 16th Thailand Astronomy Olympiad',
+		description: 'Won a Gold Medal from the 16th Thailand Astronomy Olympiad.',
+	},
+	{
+		date: '2018 May',
+		title: 'Gold Medal - 16th Thailand Astronomy Olympiad',
+		description: 'Won a Gold Medal from the 15th Thailand Astronomy Olympiad.',
+	},
+	{
+		date: '2017 April',
+		title: 'Gold Medal - 16th Thailand Astronomy Olympiad',
+		description: 'Won a Gold Medal from the 14th Thailand Astronomy Olympiad.',
+	},
+	{
+		date: '2016 May',
+		title: 'Gold Medal - 16th Thailand Astronomy Olympiad',
+		description: 'Won a Gold Medal from the 13th Thailand Astronomy Olympiad.',
+	},
+];
