8cdbe79dd850fb7258ea9587a4d21384bd652af1
Kasidis Chantharojwong <me@ckasidis.com>
2022-11-05 09:49:13 +0800

    add name

--- src/components/brand/Logo.tsx
+++ src/components/brand/Logo.tsx
@@ -1,7 +1,11 @@
 const Logo = () => {
 	return (
-		<a href="/">
+		<a href="/" className="flex items-center gap-2">
 			<img className="w-10 rounded-md" src="/profile.jpeg" />
+			<div className="text-xs font-medium text-gray-800">
+				<p>Kasidis</p>
+				<p>Chantharojwong</p>
+			</div>
 		</a>
 	);
 };
